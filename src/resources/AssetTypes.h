c1143fc9187c8820f0ddde201edc41dd3a42b1a1
pkncoder <alex.h.preston@gmail.com>
2026-05-04 03:11:06 -0400

    Another todo change (moving the hash for vertex)

--- src/resources/AssetTypes.h
+++ src/resources/AssetTypes.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <glm/glm.hpp>
+#define GLM_ENABLE_EXPERIMENTAL
+#include <glm/gtx/hash.hpp>
 
 #include <string>
 #include <vector>
@@ -28,3 +30,23 @@ struct MeshData {
 };
 
 } // namespace Engine
+
+namespace std {
+template <> struct std::hash<Engine::Vertex> {
+    size_t operator()(Engine::Vertex const &vertex) const {
+        // Using a simple bit-shifting combine method
+        size_t res = 0;
+
+        // Seperate the data evenly through the hash
+        res ^= hash<glm::vec3>()(vertex.position) + 0x9e3779b9 + (res << 6) +
+               (res >> 2);
+        res ^= hash<glm::vec3>()(vertex.normal) + 0x9e3779b9 + (res << 6) +
+               (res >> 2);
+        res ^= hash<glm::vec2>()(vertex.texCoords) + 0x9e3779b9 + (res << 6) +
+               (res >> 2);
+
+        // Retrun the final hash
+        return res;
+    }
+};
+} // namespace std
--- src/resources/ModelLoader.cpp
+++ src/resources/ModelLoader.cpp
@@ -4,31 +4,8 @@
 
 #include <string>
 #include <tiny_obj_loader.h>
-#define GLM_ENABLE_EXPERIMENTAL
-#include <glm/gtx/hash.hpp>
 
 #include <unordered_map>
-
-// TODO: Here? Can be moved by vertex definition?
-namespace std {
-template <> struct hash<Engine::Vertex> {
-    size_t operator()(Engine::Vertex const &vertex) const {
-        // Using a simple bit-shifting combine method
-        size_t res = 0;
-
-        // Seperate the data evenly through the hash
-        res ^= hash<glm::vec3>()(vertex.position) + 0x9e3779b9 + (res << 6) +
-               (res >> 2);
-        res ^= hash<glm::vec3>()(vertex.normal) + 0x9e3779b9 + (res << 6) +
-               (res >> 2);
-        res ^= hash<glm::vec2>()(vertex.texCoords) + 0x9e3779b9 + (res << 6) +
-               (res >> 2);
-
-        // Retrun the final hash
-        return res;
-    }
-};
-} // namespace std
 
 namespace Engine {
 
