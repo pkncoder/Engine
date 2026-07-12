4d646e952ffd313e2203c57ea307808d28fd140a
pkncoder <alex.h.preston@gmail.com>
2026-07-12 17:22:58 -0400

    merge

--- shaders/rasterizing/stylizing.glsl
+++ shaders/rasterizing/stylizing.glsl
@@ -2,11 +2,11 @@ const float alphaCuttoff = 0.2;
 
 const float shadowDiskRadius = 0.05;
 
-const float exposure = 0.3;
+const float exposure = 0.15;
 
 const bool toneMap = true;
 
-const vec3  fogColor = vec3(0.5, 0.4, 0.1);
+const vec3  fogColor = vec3(0.1, 0.1, 0.1);
 const float fogDensity = 0.015;
 const bool fog = true;
 
