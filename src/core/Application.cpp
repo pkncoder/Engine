961ce34cee6126f8bf084dbe1f4d7013a58f9a2c
pkncoder <alex.h.preston@gmail.com>
2026-08-03 10:50:00 -0400

    Added application back into the mix & fixed the scene loading section.

--- src/core/Application.cpp
+++ src/core/Application.cpp
@@ -1,6 +1,6 @@
 #include "Application.h"
 
-#include "../scene/Entity.h"
+#include "../resources/AssetManager.h"
 #include "../scene/SceneManager.h"
 #include "../scene/components/MaterialComponent.h"
 #include "../scene/components/TransformComponent.h"
@@ -13,6 +13,7 @@
 #include <glm/ext/vector_float3.hpp>
 
 #include <cstdint>
+#include <memory>
 
 namespace Engine {
 
@@ -135,216 +136,235 @@ void Application::run() {
 
 // TODO: temp
 
-// void Application::setupEntities() {
-//     START_PROFILE("Entity Loading");
-//
-//     auto *sceneManager = engineContext->getScene();
-//
-//     // Bunny
-//     if (0) {
-//
-//         std::vector<Entity> bunny =
-//             sceneManager->loadObjScene("assets/models/bunny.obj");
-//
-//         bunny[0].getComponent<TransformComponent>().position =
-//             glm::vec3(-1.0f, -1.2f, -4.0f);
-//     }
-//
-//     // Dragon
-//     if (0) {
-//         std::vector<Entity> dragon =
-//             sceneManager->loadObjScene("assets/models/dragon.obj");
-//
-//         dragon[0].getComponent<TransformComponent>().position =
-//             glm::vec3(1.0f, -0.6f, -4.0f);
-//     }
-//
-//     // Cat
-//     if (0) {
-//         std::vector<Entity> cat =
-//             sceneManager->loadObjScene("assets/models/cat.obj");
-//
-//         cat[0].getComponent<TransformComponent>().position =
-//             glm::vec3(0.0f, -0.6f, -4.0f);
-//     }
-//
-//     // 🗿
-//     if (0) {
-//         std::vector<Entity> moai =
-//             sceneManager->loadObjScene("assets/models/moai.obj");
-//
-//         moai[0].getComponent<TransformComponent>().position =
-//             glm::vec3(0.0f, 1.3f, -4.0f);
-//         moai[0].getComponent<TransformComponent>().rotation =
-//             glm::quat(-0.707f, 0.0f, 0.707f, 0.0f);
-//         moai[0].getComponent<TransformComponent>().scale =
-//             glm::vec3(0.14f, 0.14f, 0.14f);
-//     }
-//
-//     // Diffuse cube & emmisive cube
-//     if (0) {
-//         std::vector<Entity> cube =
-//             sceneManager->loadObjScene("assets/models/cube.obj");
-//
-//         cube[0].getComponent<MaterialComponent>().albedo =
-//             glm::vec3(0.4f, 0.2f, 0.8f);
-//         cube[0].getComponent<MaterialComponent>().roughness = 0.7;
-//         cube[0].getComponent<MaterialComponent>().metallic = 1.0;
-//
-//         cube[0].getComponent<TransformComponent>().position =
-//             glm::vec3(-1.5f, 1.3f, -4.0f);
-//         cube[0].getComponent<TransformComponent>().scale =
-//             glm::vec3(0.4f, 0.4f, 0.4f);
-//
-//         std::vector<Entity> emmissiveCube =
-//             sceneManager->loadObjScene("assets/models/cube.obj");
-//
-//         emmissiveCube[0].getComponent<TransformComponent>().position =
-//             glm::vec3(1.3f, 8.4f, -0.2f);
-//         emmissiveCube[0].getComponent<TransformComponent>().scale =
-//             glm::vec3(0.2f);
-//         emmissiveCube[0].getComponent<MaterialComponent>().albedo =
-//             glm::vec3(0.0f);
-//         emmissiveCube[0].getComponent<MaterialComponent>().emmissive =
-//             glm::vec3(2.0);
-//     }
-//
-//     // Gay Room (me)
-//     if (1) {
-//         std::vector<Entity> room =
-//             sceneManager->loadObjScene("assets/models/gayRoom.obj");
-//
-//         for (Entity entity : room) {
-//             entity.getComponent<MaterialComponent>().emmissive *=
-//                 glm::vec3(1.0f);
-//         }
-//     }
-//
-//     // Trans flag
-//     if (0) {
-//         std::vector<Entity> trans =
-//             sceneManager->loadObjScene("assets/models/trans.obj");
-//
-//         for (Entity entity : trans) {
-//             entity.getComponent<TransformComponent>().position +=
-//                 glm::vec3(-1.0f, 0.0f, 0.0f);
-//         }
-//     }
-//
-//     // Franch flag
-//     if (0) {
-//         std::vector<Entity> french =
-//             sceneManager->loadObjScene("assets/models/french.obj");
-//
-//         for (Entity entity : french) {
-//             entity.getComponent<TransformComponent>().position +=
-//                 glm::vec3(8.0f, 0.0f, 2.0f);
-//         }
-//     }
-//
-//     // THE BACKROOMS????
-//     if (0) {
-//         //
-//         https://sketchfab.com/3d-models/backrooms-v2-level-0-made-by-me-in-blender-91d707acdfce4d5d940f7cb8c25c6e31#download
-//         std::vector<Entity> backrooms =
-//             sceneManager->loadObjScene("assets/models/backrooms_level1.obj");
-//
-//         std::vector<Entity> emmissiveCube =
-//             sceneManager->loadObjScene("assets/models/cube.obj");
-//
-//         // emmissiveCube[0].getComponent<TransformComponent>().position =
-//         //     glm::vec3(80.2f, 6.5f, -116.7f);
-//         emmissiveCube[0].getComponent<TransformComponent>().position =
-//             glm::vec3(47.1f, 8.1f, -50.8f);
-//         emmissiveCube[0].getComponent<TransformComponent>().scale =
-//             glm::vec3(0.2f);
-//         emmissiveCube[0].getComponent<MaterialComponent>().albedo =
-//             glm::vec3(0.0f);
-//         emmissiveCube[0].getComponent<MaterialComponent>().emmissive =
-//             glm::vec3(2.0);
-//
-//         // 0.5/0.3 exposure
-//         // 0.02/0.015 fog density; 0.5, 0.4, 0.1 fog color
-//         // 1.5 vinette radius; 0.9 softness
-//         // 0.1, 0.1, 0.1 clear color
-//     }
-//
-//     // Breakfast room
-//     if (0) {
-//         std::vector<Entity> room =
-//             sceneManager->loadObjScene("assets/models/breakfast_room.obj");
-//
-//         std::vector<Entity> emmissiveCube =
-//             sceneManager->loadObjScene("assets/models/cube.obj");
-//
-//         emmissiveCube[0].getComponent<TransformComponent>().position =
-//             glm::vec3(-2.2f, 3.8f, -1.9f);
-//         emmissiveCube[0].getComponent<TransformComponent>().scale =
-//             glm::vec3(0.05f);
-//         emmissiveCube[0].getComponent<MaterialComponent>().albedo =
-//             glm::vec3(0.0f);
-//         emmissiveCube[0].getComponent<MaterialComponent>().emmissive =
-//             glm::vec3(0.8f, 0.7f, 0.2f);
-//
-//         // 0.7 exposure
-//         // 0.02 fog density; 0.3, 0.3, 0.3 fog color
-//         // No vinette
-//         // 0.1, 0.3, 0.5 clear color
-//     }
-//
-//     // Sponza
-//     if (0) {
-//         std::vector<Entity> sponza =
-//             sceneManager->loadObjScene("assets/models/sponza.obj");
-//
-//         std::vector<Entity> emmissiveCube =
-//             sceneManager->loadObjScene("assets/models/cube.obj");
-//
-//         emmissiveCube[0].getComponent<TransformComponent>().position =
-//             glm::vec3(1.3f, 8.4f, -0.2f);
-//         emmissiveCube[0].getComponent<TransformComponent>().scale =
-//             glm::vec3(0.05f);
-//         emmissiveCube[0].getComponent<MaterialComponent>().albedo =
-//             glm::vec3(0.0f);
-//         emmissiveCube[0].getComponent<MaterialComponent>().emmissive =
-//             glm::vec3(2.0);
-//
-//         // 0.15 exposure
-//         // 0.02 fog density; 0.5, 0.4, 0.1 fog color
-//         // 1.5 vinette radius; 0.9 softness
-//         // 0.1, 0.1, 0.1 clear color
-//     }
-//
-//     // Lost empire (Minecraft)
-//     if (0) {
-//         std::vector<Entity> lostEmpire =
-//             sceneManager->loadObjScene("assets/models/lost_empire.obj");
-//
-//         std::vector<Entity> emmissiveCube =
-//             sceneManager->loadObjScene("assets/models/cube.obj");
-//
-//         emmissiveCube[0].getComponent<TransformComponent>().position =
-//             glm::vec3(-10.7f, 22.4f, 3.1f);
-//         emmissiveCube[0].getComponent<TransformComponent>().scale =
-//             glm::vec3(0.05f);
-//         emmissiveCube[0].getComponent<MaterialComponent>().albedo =
-//             glm::vec3(0.0f);
-//         emmissiveCube[0].getComponent<MaterialComponent>().emmissive =
-//             glm::vec3(2.0);
-//
-//         // 0.7 exposure
-//         // 0.02 fog density; 0.7, 0.85, 0.98 fog color
-//         // 1.74 vinette radius; 0.9 softness
-//         // 0.1, 0.1, 0.1 clear color
-//     }
-//
-//     // oiiaioooooiai (broken texture)
-//     if (0) {
-//         std::vector<Entity> oiiaioooooiai =
-//             sceneManager->loadObjScene("assets/models/oiiaioooooiai.obj");
-//     }
-//
-//     END_PROFILE_STACKED_LOG("Entity Loading");
-// }
+void Application::setupEntities() {
+    START_PROFILE("Entity Loading");
+
+    auto *sceneManager = engineContext->getScene();
+    auto &scene = sceneManager->getScene();
+
+    // Bunny
+    if (0) {
+
+        std::vector<EntityID> bunny =
+            sceneManager->loadObjScene("assets/models/bunny.obj");
+
+        scene.getComponent<TransformComponent>(bunny[0]).position =
+            glm::vec3(-1.0f, -1.2f, -4.0f);
+    }
+
+    // Dragon
+    if (0) {
+        std::vector<EntityID> dragon =
+            sceneManager->loadObjScene("assets/models/dragon.obj");
+
+        scene.getComponent<TransformComponent>(dragon[0]).position =
+            glm::vec3(1.0f, -0.6f, -4.0f);
+    }
+
+    // Cat
+    if (0) {
+        std::vector<EntityID> cat =
+            sceneManager->loadObjScene("assets/models/cat.obj");
+
+        scene.getComponent<TransformComponent>(cat[0]).position =
+            glm::vec3(0.0f, -0.6f, -4.0f);
+    }
+
+    // 🗿
+    if (0) {
+        std::vector<EntityID> moai =
+            sceneManager->loadObjScene("assets/models/moai.obj");
+
+        scene.getComponent<TransformComponent>(moai[0]).position =
+            glm::vec3(0.0f, 1.3f, -4.0f);
+        scene.getComponent<TransformComponent>(moai[0]).rotation =
+            glm::quat(-0.707f, 0.0f, 0.707f, 0.0f);
+        scene.getComponent<TransformComponent>(moai[0]).scale =
+            glm::vec3(0.14f, 0.14f, 0.14f);
+    }
+
+    // Diffuse cube & emmisive cube
+    if (0) {
+        std::vector<EntityID> cube =
+            sceneManager->loadObjScene("assets/models/cube.obj");
+
+        scene.getComponent<TransformComponent>(cube[0]).position =
+            glm::vec3(-1.5f, 1.3f, -4.0f);
+        scene.getComponent<TransformComponent>(cube[0]).scale =
+            glm::vec3(0.4f, 0.4f, 0.4f);
+
+        std::shared_ptr<CPUMaterialData> materialCube =
+            engineContext->getAsset()->getMaterial(
+                scene.getComponent<MaterialComponent>(cube[0]).handle);
+
+        materialCube->albedo = glm::vec3(0.4f, 0.2f, 0.8f);
+        materialCube->roughness = 0.7;
+        materialCube->metallic = 1.0;
+
+        std::vector<EntityID> emissiveCube =
+            sceneManager->loadObjScene("assets/models/cube.obj");
+
+        scene.getComponent<TransformComponent>(emissiveCube[0]).position =
+            glm::vec3(1.3f, 8.4f, -0.2f);
+        scene.getComponent<TransformComponent>(emissiveCube[0]).scale =
+            glm::vec3(0.2f);
+
+        std::shared_ptr<CPUMaterialData> materialEmissiveCube =
+            engineContext->getAsset()->getMaterial(
+                scene.getComponent<MaterialComponent>(emissiveCube[0]).handle);
+
+        materialEmissiveCube->albedo = glm::vec3(0.0f);
+        materialEmissiveCube->emissive = glm::vec3(2.0);
+    }
+
+    // Gay Room (me)
+    if (1) {
+        std::vector<EntityID> room =
+            sceneManager->loadObjScene("assets/models/gayRoom.obj");
+
+        // for (EntityID entity : room) {
+        //     scene.getComponent<MaterialComponent>(entity).emmissive *=
+        //         glm::vec3(1.0f);
+        // }
+    }
+
+    // Trans flag
+    if (0) {
+        std::vector<EntityID> trans =
+            sceneManager->loadObjScene("assets/models/trans.obj");
+
+        for (EntityID entity : trans) {
+            scene.getComponent<TransformComponent>(entity).position +=
+                glm::vec3(-1.0f, 0.0f, 0.0f);
+        }
+    }
+
+    // Franch flag
+    if (0) {
+        std::vector<EntityID> french =
+            sceneManager->loadObjScene("assets/models/french.obj");
+
+        for (EntityID entity : french) {
+            scene.getComponent<TransformComponent>(entity).position +=
+                glm::vec3(8.0f, 0.0f, 2.0f);
+        }
+    }
+
+    // THE BACKROOMS????
+    if (0) {
+        // https: //
+        // sketchfab.com/3d-models/backrooms-v2-level-0-made-by-me-in-blender-91d707acdfce4d5d940f7cb8c25c6e31#download
+        std::vector<EntityID> backrooms =
+            sceneManager->loadObjScene("assets/models/backrooms_level1.obj");
+
+        std::vector<EntityID> emmissiveCube =
+            sceneManager->loadObjScene("assets/models/cube.obj");
+
+        // emmissiveCube[0].getComponent<TransformComponent>().position =
+        //     glm::vec3(80.2f, 6.5f, -116.7f);
+        scene.getComponent<TransformComponent>(emmissiveCube[0]).position =
+            glm::vec3(47.1f, 8.1f, -50.8f);
+        scene.getComponent<TransformComponent>(emmissiveCube[0]).scale =
+            glm::vec3(0.2f);
+
+        std::shared_ptr<CPUMaterialData> material =
+            engineContext->getAsset()->getMaterial(
+                scene.getComponent<MaterialComponent>(emmissiveCube[0]).handle);
+
+        material->albedo = glm::vec3(0.0f);
+        material->emissive = glm::vec3(2.0);
+
+        // 0.5/0.3 exposure
+        // 0.02/0.015 fog density; 0.5, 0.4, 0.1 fog color
+        // 1.5 vinette radius; 0.9 softness
+        // 0.1, 0.1, 0.1 clear color
+    }
+
+    // Breakfast room
+    if (0) {
+        std::vector<EntityID> room =
+            sceneManager->loadObjScene("assets/models/breakfast_room.obj");
+
+        std::vector<EntityID> emmissiveCube =
+            sceneManager->loadObjScene("assets/models/cube.obj");
+
+        scene.getComponent<TransformComponent>(emmissiveCube[0]).position =
+            glm::vec3(-2.2f, 3.8f, -1.9f);
+        scene.getComponent<TransformComponent>(emmissiveCube[0]).scale =
+            glm::vec3(0.05f);
+
+        std::shared_ptr<CPUMaterialData> material =
+            engineContext->getAsset()->getMaterial(
+                scene.getComponent<MaterialComponent>(emmissiveCube[0]).handle);
+
+        material->albedo = glm::vec3(0.0f);
+        material->emissive = glm::vec3(0.8f, 0.7f, 0.2f);
+
+        // 0.7 exposure
+        // 0.02 fog density; 0.3, 0.3, 0.3 fog color
+        // No vinette
+        // 0.1, 0.3, 0.5 clear color
+    }
+
+    // Sponza
+    if (0) {
+        std::vector<EntityID> sponza =
+            sceneManager->loadObjScene("assets/models/sponza.obj");
+
+        std::vector<EntityID> emmissiveCube =
+            sceneManager->loadObjScene("assets/models/cube.obj");
+
+        scene.getComponent<TransformComponent>(emmissiveCube[0]).position =
+            glm::vec3(1.3f, 8.4f, -0.2f);
+        scene.getComponent<TransformComponent>(emmissiveCube[0]).scale =
+            glm::vec3(0.05f);
+
+        std::shared_ptr<CPUMaterialData> material =
+            engineContext->getAsset()->getMaterial(
+                scene.getComponent<MaterialComponent>(emmissiveCube[0]).handle);
+
+        material->albedo = glm::vec3(0.0f);
+        material->emissive = glm::vec3(2.0);
+
+        // 0.15 exposure
+        // 0.02 fog density; 0.5, 0.4, 0.1 fog color
+        // 1.5 vinette radius; 0.9 softness
+        // 0.1, 0.1, 0.1 clear color
+    }
+
+    // Lost empire (Minecraft)
+    if (0) {
+        std::vector<EntityID> lostEmpire =
+            sceneManager->loadObjScene("assets/models/lost_empire.obj");
+
+        std::vector<EntityID> emmissiveCube =
+            sceneManager->loadObjScene("assets/models/cube.obj");
+
+        scene.getComponent<TransformComponent>(emmissiveCube[0]).position =
+            glm::vec3(-10.7f, 22.4f, 3.1f);
+        scene.getComponent<TransformComponent>(emmissiveCube[0]).scale =
+            glm::vec3(0.05f);
+
+        std::shared_ptr<CPUMaterialData> material =
+            engineContext->getAsset()->getMaterial(
+                scene.getComponent<MaterialComponent>(emmissiveCube[0]).handle);
+
+        material->albedo = glm::vec3(0.0f);
+        material->emissive = glm::vec3(2.0);
+
+        // 0.7 exposure
+        // 0.02 fog density; 0.7, 0.85, 0.98 fog color
+        // 1.74 vinette radius; 0.9 softness
+        // 0.1, 0.1, 0.1 clear color
+    }
+
+    // oiiaioooooiai (broken texture)
+    if (0) {
+        std::vector<EntityID> oiiaioooooiai =
+            sceneManager->loadObjScene("assets/models/oiiaioooooiai.obj");
+    }
+
+    END_PROFILE_STACKED_LOG("Entity Loading");
+}
 
 } // namespace Engine
