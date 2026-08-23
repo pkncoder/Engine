5f63d5d882f58ba480b98e4f90b341f27994e51b
pkncoder <alex.h.preston@gmail.com>
2026-08-22 22:09:54 -0400

    Weird position issue

--- debugShaders/last_VERTEX_shadow.vert.glsl
+++ debugShaders/last_VERTEX_shadow.vert.glsl
@@ -1,9 +1,12 @@
 #version 330 core
 layout (location = 0) in vec3 aPos;
 
+out vec4 FragPos;
+
 uniform mat4 uModel;
 
 void main() {
     // Just pass the world-space position to the Geometry Shader
-    gl_Position = uModel * vec4(aPos, 1.0);
+    FragPos = uModel * vec4(aPos, 1.0);
+    gl_Position = FragPos;
 }
